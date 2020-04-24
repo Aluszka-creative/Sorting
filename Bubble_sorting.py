df727b2c499d4ce09ac80a02d13f99304d777dc1
Aluszka <62706188+Aluszka-creative@users.noreply.github.com>
2020-04-24 18:46:13 +0200

    Add files via upload

---
+++ Bubble_sorting.py
@@ -0,0 +1,21 @@
+# Bubble sorting
+
+def bubble_sort(list):
+    if len(list) == 0:
+        return None
+    elif len(list) == 1:
+        return list[0]
+    else:
+        copy_list = []
+        for i_element in list:
+            copy_list.append(i_element)
+        number_of_elements = len(copy_list)
+        while number_of_elements > 1:
+            for i_index in range(number_of_elements - 1):
+                if copy_list[i_index] > copy_list[i_index + 1]:
+                    copy_list[i_index], copy_list[i_index + 1] =  copy_list[i_index + 1], copy_list[i_index]
+            number_of_elements = number_of_elements - 1
+        return copy_list
+
+list_ = [5, 1, 4, 7, 9, 3, 8, 2]
+print(bubble_sort(list_))
---
+++ Sorting.py
@@ -0,0 +1,71 @@
+# Returns the biggest element from a list
+def max(list):
+
+    if len(list) == 0:
+        return None
+    elif len(list) == 1:
+        return list[0]
+    else:
+        biggest = list[0]
+
+        for i_item in list:
+            if i_item >= biggest:
+                biggest = i_item
+
+        return biggest
+
+# Returns the smallest element from a list
+def min(list):
+    if len(list) == 0:
+        return None
+    elif len(list) == 1:
+        return list[0]
+    else:
+        smallest = list[0]
+        for i_item in list:
+            if i_item < smallest:
+                smallest = i_item
+
+    return smallest
+# Function which checks if element is in the list
+def contains(list, x):
+    for i_item in list:
+
+        if i_item == x:
+            return 1
+    return 0
+
+def sort(list, from_bigger_or_smaller):
+    if len(list) == 0:
+        return None
+    elif len(list) == 1:
+        return list[0]
+    else:
+        if from_bigger_or_smaller == 'from_bigger':
+            copy_list = []
+            sorted_list = []
+            for i_element in list:
+                copy_list.append(i_element)
+            for i_index in range(0, len(list)):
+                max_element = max(copy_list)
+                sorted_list.append(max_element)
+                copy_list.remove(max_element)
+            return sorted_list
+        elif from_bigger_or_smaller == 'from_smaller':
+            copy_list = []
+            sorted_list = []
+            for i_element in list:
+                copy_list.append(i_element)
+            for i_index in range(0, len(list)):
+                min_element = min(copy_list)
+                sorted_list.append(min_element)
+                copy_list.remove(min_element)
+            return sorted_list
+
+
+
+
+
+
+list_ = [3, 5, 4, 1, 3]
+print(sort(list_, 'from_smaller'))
