bf059c3a7b4a8521365254ba9d59cf6cffe6b159
Aluszka <62706188+Aluszka-creative@users.noreply.github.com>
2020-04-24 21:56:20 +0200

    Update Bubble_sorting.py

--- Bubble_sorting.py
+++ Bubble_sorting.py
@@ -11,10 +11,10 @@ def bubble_sort(list):
             copy_list.append(i_element)
         number_of_elements = len(copy_list)
         while number_of_elements > 1:
-            for i_index in range(number_of_elements - 1):
-                if copy_list[i_index] > copy_list[i_index + 1]:
-                    copy_list[i_index], copy_list[i_index + 1] =  copy_list[i_index + 1], copy_list[i_index]
-            number_of_elements = number_of_elements - 1
+            for i_index in range(number_of_elements-1):
+                if copy_list[i_index] > copy_list[i_index+1]:
+                    copy_list[i_index], copy_list[i_index+1] = copy_list[i_index+1], copy_list[i_index]
+            number_of_elements = number_of_elements-1
         return copy_list
 
 list_ = [5, 1, 4, 7, 9, 3, 8, 2]
